84ba8102e5dd1fde27b82d5299dec087a51c8d4b
hp <hp@DESKTOP-P9AVAHE>
2023-09-24 15:03:20 +0530

    update for app 1

--- sabjiApplication/src/main/java/com/sabji/contoller/GetController.java
+++ sabjiApplication/src/main/java/com/sabji/contoller/GetController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sabji.entity.DriverDetails;
 import com.sabji.entity.FarmerInfo;
 import com.sabji.entity.FarmerInfoEntity;
-import com.sabji.entity.Items;
 import com.sabji.entity.VegetableEntity;
 import com.sabji.model.BasicDetailsDTO;
 import com.sabji.model.ResponseWithList;
@@ -21,66 +20,61 @@ import com.sabji.services.ItemService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
- 
-
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("api")
-@Tag(name="For-Getting-Data")
+@Tag(name = "For-Getting-Data")
 public class GetController {
-	
+
 	@Autowired
 	ItemService itemService;
-	
+
 	@GetMapping("/findItems")
-	public ResponseEntity<?> findAllItems(){
-		
-		List<BasicDetailsDTO> items =	itemService.getallItems();
-		
-		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", items) ;
-		
-	}
-	
-	@GetMapping("/findVegatable")
-	public ResponseEntity<?> findVegatableDetail(){
-		
-		List<VegetableEntity> vegetableEntities	 = itemService.getallVegetableDetail();
-		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", vegetableEntities) ;
-		
-		
-		
+	public ResponseEntity<Object> findAllItems() {
+
+		List<BasicDetailsDTO> items = itemService.getallItems();
+
+		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", items);
+
 	}
 
-	
+	@GetMapping("/findVegatable")
+	public ResponseEntity<?> findVegatableDetail() {
+
+		List<VegetableEntity> vegetableEntities = itemService.getallVegetableDetail();
+		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", vegetableEntities);
+
+	}
+
 //	@GetMapping("/findFarmer")
 //	public ResponseEntity<?> findFarmerDetail(){
-//		
+//
 //		List<FarmerInfoEntity>  farmerInfoEntities =  itemService.getallFarmardetail();
 //		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", farmerInfoEntities) ;
-//		
+//
 //	}
 	@GetMapping("/findDeliveryPartner")
-	public ResponseEntity<?> findDeliveryPartner(){
-		
-		List<FarmerInfoEntity>  farmerInfoEntities =  itemService.getallFarmardetail();
-		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", farmerInfoEntities) ;
-		
+	public ResponseEntity<?> findDeliveryPartner() {
+
+		List<FarmerInfoEntity> farmerInfoEntities = itemService.getallFarmardetail();
+		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", farmerInfoEntities);
+
 	}
-	
+
 	@GetMapping("/findFarmer")
-	public ResponseEntity<?> findfarmer(){
-		
+	public ResponseEntity<?> findfarmer() {
+
 		List<FarmerInfo> farmerinformation = itemService.getallFarmerinfo();
-		
-		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", farmerinformation) ;
+
+		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", farmerinformation);
 	}
 
 	@GetMapping("/findDriverDetails")
-	public ResponseEntity<?> findDriver(){
-		
-		List<DriverDetails> driverdetails =  itemService.getdriverdetailsinfo();
-		
-		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", driverdetails) ;
+	public ResponseEntity<?> findDriver() {
+
+		List<DriverDetails> driverdetails = itemService.getdriverdetailsinfo();
+
+		return new ResponseWithList().generateResponse("provide", HttpStatus.OK, "", driverdetails);
 	}
 
 }
