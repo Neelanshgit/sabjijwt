274d9ac3157313544e425d26c60eb011d9f358a6
GORANK <GORANK@192.168.71.1>
2023-12-19 20:58:05 +0530

    gorank commit 1

--- sabjiApplication/src/main/java/com/sabji/model/ProfileModel.java
+++ sabjiApplication/src/main/java/com/sabji/model/ProfileModel.java
@@ -5,6 +5,40 @@ public class ProfileModel {
 	private String mobileno;
 
 	private String email;
+	private String username;
+	private String aadharNo;
+	 
+	
+	private String flagofuser;
+
+
+	
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	
+
+
+	
 
 	public String getMobileno() {
 		return mobileno;
@@ -14,12 +48,25 @@ public class ProfileModel {
 		this.mobileno = mobileno;
 	}
 
-	public String getEmail() {
-		return email;
+
+	public String getAadharNo() {
+		return aadharNo;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setAadharNo(String aadharNo) {
+		this.aadharNo = aadharNo;
 	}
 
+
+	public String getFlagofuser() {
+		return flagofuser;
+	}
+
+
+	public void setFlagofuser(String flagofuser) {
+		this.flagofuser = flagofuser;
+	} 
+
+	
 }
